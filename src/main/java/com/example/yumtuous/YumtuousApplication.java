40a214918d880a1bd20f069060ec5e1043098bdf
Kapil Sharma (Lazada Group) <kapil.sharma@LAZ-SG-L-M-0234.local>
2018-08-02 23:34:10 +0800

    first commit

---
+++ src/main/java/com/example/yumtuous/YumtuousApplication.java
@@ -0,0 +1,12 @@
+package com.example.yumtuous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumtuousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YumtuousApplication.class, args);
+	}
+}
